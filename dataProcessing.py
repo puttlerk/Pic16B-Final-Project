1fd51abf76a29c9ef5ecee142a85489039a157c8
Artem <feugok@gmail.com>
2024-05-22 16:36:26 -0700

    Create model class, adds training and testing function

--- dataProcessing.py
+++ dataProcessing.py
@@ -5,17 +5,19 @@ from torchvision import transforms, datasets
 
 # Define transformations (TODO figure out proper transformations)
 transform = transforms.Compose([
-    transforms.Resize((256, 256)),
+    transforms.Resize((32,32)),
     transforms.ToTensor(),
-    #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 
 # Path to  training images
 local_repo_path = "ASL_Data/asl_alphabet_train/asl_alphabet_train"
 
+
 # Create PyTorch dataset using ImageFolder
 train_dataset = datasets.ImageFolder(root=local_repo_path, transform=transform)
+
 
 # Calculates how many training examples should be in train and validation datasets
 total_size = len(train_dataset)
@@ -25,7 +27,9 @@ val_size = total_size - train_size
 # Splits the data into train_data and validation_data
 train_data, val_data = random_split(train_dataset, [train_size, val_size])
 
+print(type(val_data))
+
 # Dataloader for out train and validation sets
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_data, batch_size=32, shuffle=True)
 
---
+++ models.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from dataProcessing import train_loader, val_loader
+
+
+
+# Classic model architecture of LeNet5
+class LeNet5(nn.Module):
+
+    def __init__(self):
+
+        super(LeNet5,self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3,out_channels=6, kernel_size=(5,5), stride=1, padding=0)
+        self.pool1 = nn.MaxPool2d(kernel_size=(2,2), stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6,out_channels=16, kernel_size=(5,5), stride=1, padding=0)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.fc3 = nn.Linear(400,120)
+        self.fc4 = nn.Linear(120, 84)
+        self.fc5 = nn.Linear(84, 29)
+
+
+    def forward(self, x):
+        x = F.relu(self.pool1(self.conv1(x)))
+        x = F.relu(self.pool2(self.conv2(x)))
+
+        x = x.view(-1,400)
+
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
+
+        return x
