491d344687ebaf8106e46f0cd0323d0fedef026d
Karl Puttler <puttlerk@gmail.com>
2024-05-30 20:15:11 -0700

    Extract clean

--- videomodel/extract_test.py
+++
@@ -1,8 +0,0 @@
-import json
-from extract import instance_videos
-
-with open("./WLASL_v0.3.json") as file: 
-    data = json.load(file)
-
-for datum in data:
-    instance_videos(datum["gloss"], datum["instances"], "test")
