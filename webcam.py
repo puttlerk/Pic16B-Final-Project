9bf41a009d0b9ee07458802969fc5c0a1e31cb3b
Karl Puttler <puttlerk@gmail.com>
2024-06-11 13:55:05 -0700

    ImageRecognitionPath.pth

--- webcam.py
+++ webcam.py
@@ -29,31 +29,43 @@ def capture_loop(quit_flag):
 
     assert(quit_flag.value == 0) # quit_flag should be 0 when we start
 
-    cap = cv2.Videocapter(0) # get file descriptor for webcam
+    cap = cv2.VideoCapture(0) # get file descriptor for webcam
 
     while (not quit_flag and cap.isOpened()): # while the user hasn't quite and the webcam is open
 
         ret, frame = cap.read() # read a frame
         if (ret): # if the read was successful
             cv2.imshow("frame", frame) # show the frame
+            print(type(frame))
         
     cap.release() # close the webcam file descriptor
     cv2.destroyAllWindows() # close the windows we made
+
+
+def box_on_frame(frame):
+    """
+    Puts a box in the frame indicating where the model will look
+
+    args:
+        frame: the cv2 frame to 
+    """
 
 def main_loop():
     """
     main loop for the application
     """
 
-    if __name__ == "__main__":
+    if (__name__ == "__main__"):
 
         # initialize quit_flag
         quit_flag = Value('i', 0)
 
         # start the quit_loop and capture loop
-        quit_loop = Process(target = quit_loop, args = [quit_flag]).start()
-        capture_loop = Process(target = capture_loop, args = [quit_flag]).start()
+        q_loop = Process(target = quit_loop, args = (quit_flag,)).start()
+        c_loop = Process(target = capture_loop, args = (quit_flag,)).start()
 
         # join with quit_loop and capture_loop
-        capture_loop.join()
-        quit_loop.join()
+        c_loop.join()
+        q_loop.join()
+
+main_loop()
