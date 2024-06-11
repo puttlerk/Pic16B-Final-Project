f3bf0acbc29601672e400c9f7c65098ce2fcd5bf
Karl Puttler <puttlerk@gmail.com>
2024-06-11 13:23:25 -0700

    Update dataProcessing.py
    Spammed some transforms

--- dataProcessing.py
+++ dataProcessing.py
@@ -10,6 +10,10 @@ from torchvision import transforms, datasets
 train_transform = transforms.Compose([
     transforms.Resize((32,32)),
     transforms.RandomRotation(degrees=15),
+    transforms.RandomHorizontalFlip(p=0.2),
+    transforms.RandomVerticalFlip(p=0.2),
+    transforms.RandomAffine(degrees=(-15, 15), p=0.2),
+    transforms.RandomPerspective(p=0.2),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
